c8a963752a46302e4024932b4cc484ff2741d833
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2024-02-06 20:29:46 +0700

    App structure

---
+++ lib/app/todo_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/screens/screens.dart';
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomeScreen());
+  }
+}
